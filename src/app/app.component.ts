78b124ebcbf7a0d7316f5c84003e2e26ba49a657
NeverMyth <hemersondc96@gmail.com>
2023-03-02 15:40:40 +0000

    lab5 complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { defaultIfEmpty } from 'rxjs';
+import { isBlock } from 'typescript';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'data-app';
+
+  count : number = 0;
+  look : string = "look at the star"
+  hidden:boolean=true;
+
+  presidents: string[] = ["Higgins",
+    "McAleese", "Robinson", "Hillery"];
+
+president: any[] =
+[{name: "Higgins", term:"2011-"},
+{name: "McAleese", term:"1997-2011"},
+{name: "Robinson", term:"1990-1997"},
+{name: "Hillery", term:"1976-1990"},
+{name: "O Dalaigh", term:"1974-1976"}];
+
+
+  onClick(){
+    this.count++;
+  }
+  disClick(){
+    this.count--;
+
+    if (this.count<=-1){
+      this.count++;
+    }
+  }
+  onDoubleClick(){
+  
+    if(this.hidden===false){
+      this.hidden=true;
+    }else{
+      this.hidden=false;
+    }
+  }
+
+
 }
